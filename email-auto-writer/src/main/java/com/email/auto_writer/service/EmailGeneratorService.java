ec698f0f703c11e7afd1ce38a2b316f63fb8ea11
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-03-05 17:35:00 +0100

    Build the prompt

--- email-auto-writer/src/main/java/com/email/auto_writer/service/EmailGeneratorService.java
+++ email-auto-writer/src/main/java/com/email/auto_writer/service/EmailGeneratorService.java
@@ -9,4 +9,17 @@ public class EmailGeneratorService {
     public String generateEmailReply(EmailRequest emailRequest){
         return null;
     }
+
+    private String buildPrompt(EmailRequest emailRequest){
+        //Build the prompt
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following content. Please don't generate a subject line");
+
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append("tone");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+
+        return prompt.toString();
+    }
 }
