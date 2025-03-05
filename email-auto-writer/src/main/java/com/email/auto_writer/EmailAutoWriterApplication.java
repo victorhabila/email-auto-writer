f8b8235f692f03f01fd282fe3cc094925351d6c9
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-03-05 16:42:54 +0100

    First commit email auto writer message

---
+++ email-auto-writer/src/main/java/com/email/auto_writer/EmailAutoWriterApplication.java
@@ -0,0 +1,13 @@
+package com.email.auto_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAutoWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAutoWriterApplication.class, args);
+	}
+
+}
---
+++ email-auto-writer/src/test/java/com/email/auto_writer/EmailAutoWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.auto_writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAutoWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
