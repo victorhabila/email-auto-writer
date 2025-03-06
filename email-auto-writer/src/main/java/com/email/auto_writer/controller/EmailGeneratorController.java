0bef37fbf5ed78d74502942fb1d70f38c4066194
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-03-06 14:58:15 +0100

    Update on the EmailGeneratorService class

--- email-auto-writer/src/main/java/com/email/auto_writer/controller/EmailGeneratorController.java
+++ email-auto-writer/src/main/java/com/email/auto_writer/controller/EmailGeneratorController.java
@@ -3,6 +3,8 @@ package com.email.auto_writer.controller;
 
 import com.email.auto_writer.model.EmailRequest;
 import com.email.auto_writer.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +26,6 @@ public class EmailGeneratorController {
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
 
         String response =emailGeneratorService.generateEmailReply(emailRequest);
-        return  ResponseEntity.ok("");
+        return  ResponseEntity.ok(response);
     }
 }
